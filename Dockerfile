f81583c7ca1f096324fdba903a633f4cdf0e34c6
Avanire <drtony@yandex.ru>
2023-06-30 18:58:54 +0600

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM node:16.15.1
-
-WORKDIR /app
-COPY package.json ./
+RUN mkdir "app"
+WORKDIR app
+COPY . .
 RUN npm install
-COPY . /app
 RUN npm run build
-
 EXPOSE 3000
 CMD ["node", "server.js"]
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./src/'));
+app.use(express.static('./dist/'));
 
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console
